12965a4164d92ad2008c0b3cae68681bac51ed9a
kvietist <qaxramonovxabibillo@gmail.com>
2026-07-19 17:21:01 +0500

    add CORSmiddleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import users, auth, diaries
 app = FastAPI()
 app.include_router(users.router)
@@ -9,3 +10,12 @@ app.include_router(diaries.router)
 async def home():
     return {"message": "api is online and ready for requests"}
     
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
